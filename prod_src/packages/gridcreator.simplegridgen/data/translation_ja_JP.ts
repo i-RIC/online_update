94507efbc41e023e0d012621c6391bb50c33cd51
Keisuke Inoue <kskinoue@gmail.com>
2022-09-05 19:12:12 +0900

    The following packages are updated for prod version: * gridcreator.arc_grigen_cp * gridcreator.simplegridgen

---
+++ prod_src/packages/gridcreator.simplegridgen/data/translation_ja_JP.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>Simple Grid Generator</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Simple Channel Shape</source>
+            <translation>水路形状</translation>
+        </message>
+        <message>
+            <source>Streamwise Geometry</source>
+            <translation>流下方向形状</translation>
+        </message>
+        <message>
+            <source>Channel Length (m)</source>
+            <translation>水路延長(m)</translation>
+        </message>
+        <message>
+            <source>Channel Width (m)</source>
+            <translation>水路幅(m)</translation>
+        </message>
+        <message>
+            <source>Channel Slope (m/m)</source>
+            <translation>水路の勾配(m/m)</translation>
+        </message>
+        <message>
+            <source>Channel Crossing Angle (degrees)</source>
+            <translation>蛇行角(度)</translation>
+        </message>
+        <message>
+            <source>Number of Meander Wavelengths</source>
+            <translation>蛇行波数</translation>
+        </message>
+        <message>
+            <source>Computational Grid</source>
+            <translation>計算格子</translation>
+        </message>
+        <message>
+            <source>Number of stream-wise nodes</source>
+            <translation>流下方向格子点の数</translation>
+        </message>
+        <message>
+            <source>Number of cross-stream nodes</source>
+            <translation>横断方向格子点の数</translation>
+        </message>
+        <message>
+            <source>Cross-sectional Geometry</source>
+            <translation>横断面形状</translation>
+        </message>
+        <message>
+            <source>Cross-section Type</source>
+            <translation>横断面形状の種類</translation>
+        </message>
+        <message>
+            <source>Flat</source>
+            <translation>平坦</translation>
+        </message>
+        <message>
+            <source>Parabolic</source>
+            <translation>放物線状</translation>
+        </message>
+        <message>
+            <source>Flat Bed Depth (m)</source>
+            <translation>平坦河床の深さ(m)</translation>
+        </message>
+        <message>
+            <source>Parabolic Bank Height (m)</source>
+            <translation>放物線状の土手の高さ(m)</translation>
+        </message>
+        <message>
+            <source>Parabolic Maimum Depth (m)</source>
+            <translation>放物線状河床の最大深さ(m)</translation>
+        </message>
+        <message>
+            <source>Point Bar Geometry</source>
+            <translation>蛇行州の形状</translation>
+        </message>
+        <message>
+            <source>Point bar amplitude (0-1)</source>
+            <translation>蛇行州高の振幅(0-1)</translation>
+        </message>
+        <message>
+            <source>Point bar - bend apex phase difference (degrees) (+downstream)</source>
+            <translation>蛇行州と蛇行頂点の位相差(度)(流下方向が正)</translation>
+        </message>
+        <message>
+            <source>IMax * JMax must be smaller than 100,000.</source>
+            <translation></translation>
+        </message>
+    </context>
+</TS>
